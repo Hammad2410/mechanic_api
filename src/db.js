639d54f15be4d627d050cbf19c8ed147f1661955
hammad saeed <hammad.saeed2410@gmail.com>
2020-10-12 14:43:35 +0500

    db updated

--- src/db.js
+++ src/db.js
@@ -3,12 +3,12 @@ var { Client } = require('pg');
 //const connectionString = 'postgres://postgres:root@localhost:5432/moneyUp'
 
 var con = new Client({
-    user: "postgres",
-    password: "root1234",
-    database: "postgres",
+    user: "hmuyeifesnsrkh",
+    password: "c4b721c0a7c85c6c7bba6a73410dc14b6fa93640e727848377079a61f535ea5a",
+    database: "d3kpet623jfn9g",
     port: 5432,
-    host: "mechaniczuse.c3hwmtfmlowy.us-east-2.rds.amazonaws.com",
-    //ssl: { rejectUnauthorized: false }
+    host: "ec2-54-90-68-208.compute-1.amazonaws.com",
+    ssl: { rejectUnauthorized: false }
 
 });
 
