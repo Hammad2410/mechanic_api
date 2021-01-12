ae20b6dff4dcf8153249679174e1e404726f4e79
hammad saeed <hammad.saeed2410@gmail.com>
2021-01-12 13:29:59 +0500

    server down

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-var app = require('./src/server')
-var cors = require('cors')
+// var app = require('./src/server')
+// var cors = require('cors')
 
-const signinRoute = require('./src/Route/signin')
-const newaccountRoute = require('./src/Route/newaccount')
-const homeRoute = require('./src/Route/home')
-const reviewRoute = require('./src/Route/review')
-const adminRoute = require('./src/Route/admin')
+// const signinRoute = require('./src/Route/signin')
+// const newaccountRoute = require('./src/Route/newaccount')
+// const homeRoute = require('./src/Route/home')
+// const reviewRoute = require('./src/Route/review')
+// const adminRoute = require('./src/Route/admin')
 
-app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-    res.header("Access-Control-Allow-Methods", "GET, POST", "PUT");
-    next();
-});
+// app.all('/*', function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+//     res.header("Access-Control-Allow-Methods", "GET, POST", "PUT");
+//     next();
+// });
 
-app.use(cors())
-app.use('/services', homeRoute)
-app.use('/profile', signinRoute)
-app.use('/login', newaccountRoute)
-app.use('/rate', reviewRoute)
-app.use('/admin', adminRoute)
+// app.use(cors())
+// app.use('/services', homeRoute)
+// app.use('/profile', signinRoute)
+// app.use('/login', newaccountRoute)
+// app.use('/rate', reviewRoute)
+// app.use('/admin', adminRoute)
 
 
 
-app.get('/api', (req, res) => {
-    res.send({
-        success: true,
-        message: "server is down"
-    })
-})
+// app.get('/api', (req, res) => {
+//     res.send({
+//         success: true,
+//         message: "server is down"
+//     })
+// })
 
 //app.get('/', (req, res) => {
 //    res.sendFile(__dirname + '/index.html');
